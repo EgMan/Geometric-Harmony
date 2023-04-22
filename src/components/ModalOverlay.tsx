0c9eba75f8bb40857ec6bb167f6ce35358f4b605
Antonio Woods <antonio95woods@gmail.com>
2023-04-22 13:30:01 -0700

    Refactor out modal overlay from widget and various style tweaks

---
+++ src/components/ModalOverlay.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Layer, Stage } from "react-konva";
+import { Html } from "react-konva-utils";
+import { ThemeProvider, createTheme, css } from "@mui/material";
+import { green, purple } from "@mui/material/colors";
+import { useSpring as useSpring_web, animated as animated_web } from "@react-spring/web";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: green["A700"],
+            light: purple[300],
+            dark: purple[900],
+        },
+    }
+});
+
+type Props = {
+    isVisible: boolean,
+    setIsVisible: React.Dispatch<React.SetStateAction<boolean>>,
+    htmlContent?: JSX.Element,
+    canvasContent?: JSX.Element,
+}
+
+function ModalOverlay(props: Props) {
+    // const overlayTransition = useTransition_web(true, {
+    //     from: { opacity: 0 },
+    //     enter: { opacity: 1 },
+    //     leave: { opacity: 0 },
+    //     config: { duration: 750 }
+    // });
+    const overlayProps = useSpring_web({ opacity: props.isVisible ? 1 : 0 })
+    return <Html divProps={{ id: props.isVisible ? "overlay" : "overlay-no-pointer-events" }} >
+        {/* <div id="backdrop-blur"> */}
+        < animated_web.div id="backdrop-blur" style={overlayProps}>
+            {
+                props.canvasContent &&
+                <Stage width={window.innerWidth} height={window.innerHeight}>
+                    <Layer>
+                        {props.canvasContent}
+                    </Layer>
+                </Stage>
+            }
+            {
+                props.htmlContent &&
+                <ThemeProvider theme={theme}>
+                    <div id="click-back-div" onClick={() => props.setIsVisible(false)}>
+                        <div id="overlay-content" onClick={(e) => e.stopPropagation()}>
+                            {props.htmlContent}
+                        </div>
+                    </div>
+                </ThemeProvider>
+            }
+        </animated_web.div>
+        {/* </div> */}
+    </Html >
+}
+
+export default ModalOverlay;
--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Circle, Group, Layer, Stage } from "react-konva";
+import { Circle, Group, Layer, Stage, Text } from "react-konva";
 import { animated, useSpring, useTransition } from '@react-spring/konva';
 import { Html } from "react-konva-utils";
 import { ThemeProvider, createTheme } from "@mui/material";
 import { green, purple } from "@mui/material/colors";
+import ModalOverlay from "./ModalOverlay";
 
 type Props = {
     children?: React.ReactNode
@@ -20,7 +21,7 @@ function Widget(props: Props) {
     const [draggedY, setDraggedY] = React.useState(0);
 
     const [contextMenuOpen, setContextMenuUpen] = React.useState(false);
-    const contextMenuProps = useSpring({ opacity: contextMenuOpen ? 0.75 : 0.25, radius: contextMenuOpen ? 15 : 10 });
+    const contextMenuProps = useSpring({ opacity: contextMenuOpen ? 0.1 : 0, radius: contextMenuOpen ? 15 : 10 });
 
     const [isSettingsOverlayVisible, setIsSettingsOverlayVisible] = React.useState(false);
 
@@ -43,7 +44,7 @@ function Widget(props: Props) {
 
     return (
         <div>
-            {isSettingsOverlayVisible &&
+            {/* {isSettingsOverlayVisible &&
                 <Html divProps={{ id: "overlay" }}>
                     <Stage width={window.innerWidth} height={window.innerHeight}>
                         <Layer>
@@ -62,7 +63,30 @@ function Widget(props: Props) {
                         </div>
                     </ThemeProvider>
                 </Html>
-            }
+            } */}
+
+            <ModalOverlay
+                isVisible={isSettingsOverlayVisible}
+                setIsVisible={setIsSettingsOverlayVisible}
+                htmlContent={
+                    // <table border={1} rules="rows">
+                    <div>
+                        {/* <table>
+                            <th colSpan={10}>test</th>
+                            <tr>test</tr>
+                        </table> */}
+                        <table>
+                            {props.settingsRows}
+                        </table>
+                    </div>
+                }
+                canvasContent={
+                    isSettingsOverlayVisible ? (<Group x={props.x + draggedX} y={props.y + draggedY}>
+                        {props.children}
+                    </Group>) : undefined
+                }
+            />
+            {/* … */}
 
             <Group x={props.x} y={props.y}>
                 {mainGroupTransition(
@@ -74,8 +98,22 @@ function Widget(props: Props) {
                 )}
                 <Group draggable x={props.contextMenuX} y={props.contextMenuY} onDragMove={a => { setDraggedX(a.currentTarget.x() - props.contextMenuX); setDraggedY(a.currentTarget.y() - props.contextMenuY); }}>
                     {/* @ts-ignore: https://github.com/pmndrs/react-spring/issues/1515 */}
-                    <animated.Circle radius={15} {...contextMenuProps} fill={"black"}></animated.Circle>
+                    <animated.Circle radius={15} {...contextMenuProps} fill={"white"}></animated.Circle>
                     <Circle radius={15} opacity={0} onMouseEnter={() => setContextMenuUpen(true)} onMouseLeave={() => setContextMenuUpen(false)} onTouchStart={() => setIsSettingsOverlayVisible(true)} onClick={() => setIsSettingsOverlayVisible(true)} onContextMenu={(e) => { setIsSettingsOverlayVisible(true); e.currentTarget.preventDefault() }}></Circle>
+                    <Text
+                        bold={true}
+                        listening={false}
+                        opacity={1}
+                        width={40}
+                        height={40}
+                        x={-20}
+                        y={-23}
+                        text={'…'}
+                        fill="white"
+                        fontSize={16}
+                        fontFamily='monospace'
+                        align="center"
+                        verticalAlign="middle" />
                 </Group>
             </Group>
         </div>
