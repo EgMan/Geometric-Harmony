539d189dd867da2ea534dc5a2c68fb2e01bccb65
Antonio Woods <antonio95woods@gmail.com>
2024-01-13 16:47:18 -0800

    Added the note displays on icos

--- src/toys/Icosahedron.tsx
+++ src/toys/Icosahedron.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { WidgetComponentProps } from '../view/Widget';
 import { Point3D } from '../utils/Utils3D';
-import { smallGold } from '../utils/Utils';
+import { blendColors, smallGold } from '../utils/Utils';
 import Wireframe, { WireframeLine, WireframePoint } from './Wireframe';
+import { NoteSet, normalizeToSingleOctave, useHomeNote, useNoteDisplays, useNoteSet } from '../sound/NoteProvider';
+import { useAppTheme } from '../view/ThemeManager';
 
 type Props = {
     width: number,
@@ -10,22 +12,88 @@ type Props = {
 } & WidgetComponentProps
 
 function Icosahedron(props: Props) {
-    const [points, setPoints] = React.useState<WireframePoint[]>([
-        { location3D: { x: 1, y: smallGold, z: 0 }, text: "0" },
-        { location3D: { x: 1, y: -smallGold, z: 0 }, text: "1" },
-        { location3D: { x: -1, y: -smallGold, z: 0 }, text: "2" },
-        { location3D: { x: -1, y: smallGold, z: 0 }, text: "3" },
+    const homeNote = useHomeNote();
+    const activeNotes = useNoteSet(NoteSet.Active).notes;
+    const noteDisplays = useNoteDisplays();
+    const { colorPalette } = useAppTheme()!;
 
-        { location3D: { x: smallGold, y: 0, z: 1 }, text: "4" },
-        { location3D: { x: -smallGold, y: 0, z: 1 }, text: "5" },
-        { location3D: { x: -smallGold, y: 0, z: -1 }, text: "6" },
-        { location3D: { x: smallGold, y: 0, z: -1 }, text: "7" },
+    const idxToNote = React.useMemo(() => {
+        return [0, 1, 6, 7, 3, 5, 9, 11, 4, 8, 10, 2].map(idx => normalizeToSingleOctave(idx + (homeNote ?? 0)));
+    }, [homeNote]);
 
-        { location3D: { x: 0, y: 1, z: smallGold }, text: "8" },
-        { location3D: { x: 0, y: 1, z: -smallGold }, text: "9" },
-        { location3D: { x: 0, y: -1, z: -smallGold }, text: "10" },
-        { location3D: { x: 0, y: -1, z: smallGold }, text: "11" },
-    ]);
+    // const [points, setPoints] = React.useState<WireframePoint[]>([
+    //     { location3D: { x: 1, y: smallGold, z: 0 }, text: "0:" + idxToNote[0] },
+    //     { location3D: { x: 1, y: -smallGold, z: 0 }, text: "1:" + idxToNote[1] },
+    //     { location3D: { x: -1, y: -smallGold, z: 0 }, text: "2:" + idxToNote[2] },
+    //     { location3D: { x: -1, y: smallGold, z: 0 }, text: "3:" + idxToNote[3] },
+
+    //     { location3D: { x: smallGold, y: 0, z: 1 }, text: "4:" + idxToNote[4] },
+    //     { location3D: { x: -smallGold, y: 0, z: 1 }, text: "5:" + idxToNote[5] },
+    //     { location3D: { x: -smallGold, y: 0, z: -1 }, text: "6:" + idxToNote[6] },
+    //     { location3D: { x: smallGold, y: 0, z: -1 }, text: "7:" + idxToNote[7] },
+
+    //     { location3D: { x: 0, y: 1, z: smallGold }, text: "8:" + idxToNote[8] },
+    //     { location3D: { x: 0, y: 1, z: -smallGold }, text: "9:" + idxToNote[9] },
+    //     { location3D: { x: 0, y: -1, z: -smallGold }, text: "10:" + idxToNote[10] },
+    //     { location3D: { x: 0, y: -1, z: smallGold }, text: "11:" + idxToNote[11] },
+    // ]);
+
+    const points: WireframePoint[] = React.useMemo(() => {
+        const out: WireframePoint[] = [
+            { x: 1, y: smallGold, z: 0 },
+            { x: 1, y: -smallGold, z: 0 },
+            { x: -1, y: -smallGold, z: 0 },
+            { x: -1, y: smallGold, z: 0 },
+
+            { x: smallGold, y: 0, z: 1 },
+            { x: -smallGold, y: 0, z: 1 },
+            { x: -smallGold, y: 0, z: -1 },
+            { x: smallGold, y: 0, z: -1 },
+
+            { x: 0, y: 1, z: smallGold },
+            { x: 0, y: 1, z: -smallGold },
+            { x: 0, y: -1, z: -smallGold },
+            { x: 0, y: -1, z: smallGold },
+        ].map((p, i) => {
+            const note = idxToNote[i];
+            const channelDisplay = noteDisplays.normalized[note]?.map((noteDisplay) => noteDisplay.color!);
+            const isActiveNote = activeNotes.has(normalizeToSingleOctave(note))
+            console.log("what", activeNotes, note, isActiveNote);
+            const isBeingChannelDisplayed = (channelDisplay?.length ?? 0) > 0;
+            let color = colorPalette.Widget_Primary;
+            let circleRadius = 2.5;
+            let outlineColor = undefined;
+            let opacity = 1;
+            let moreText = "";
+            if (isBeingChannelDisplayed) {
+                circleRadius = 5;
+                color = blendColors(channelDisplay ?? []) ?? "rgba(0,0,0,0)";
+            }
+            else if (homeNote === normalizeToSingleOctave(note)) {
+                circleRadius = 3;
+                color = colorPalette.Note_Home;
+            }
+            else if (isActiveNote) {
+                circleRadius = 2.5;
+                color = colorPalette.Note_Active;
+            } else {
+                color = colorPalette.Main_Background;
+                circleRadius = 2.5;
+                outlineColor = colorPalette.Widget_Primary;
+                moreText += "fuck";
+            }
+            return {
+                color,
+                opacity,
+                outlineColor,
+                location3D: p,
+                // text: i,
+                // text: `${moreText}${note}${isActiveNote ? "A" : ""}${isBeingChannelDisplayed ? "C" : ""}`,
+                radius: circleRadius,
+            }
+        });
+        return out;
+    }, [activeNotes, colorPalette.Main_Background, colorPalette.Note_Active, colorPalette.Note_Home, colorPalette.Widget_Primary, homeNote, idxToNote, noteDisplays.normalized]);
 
     const [lines, setLines] = React.useState<WireframeLine[]>([
         { start: 0, end: 1 },
